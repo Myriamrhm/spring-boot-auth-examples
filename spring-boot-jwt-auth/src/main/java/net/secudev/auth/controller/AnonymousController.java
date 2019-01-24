293e8ca653a35b3337269fc416ef55e20b9eaf43
SecuDev <whatever>
2019-01-24 19:48:38 +0100

    post vs get

--- spring-boot-jwt-auth/src/main/java/net/secudev/auth/controller/AnonymousController.java
+++ spring-boot-jwt-auth/src/main/java/net/secudev/auth/controller/AnonymousController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class AnonymousController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@GetMapping("jwt")
+	@PostMapping("jwt")
 	public ResponseEntity<?> getJWT(@RequestBody JSONCredential cred, HttpServletRequest request) throws Exception {
 
 		if (!utilisateurs.existsByLogin(cred.getLogin())) {
