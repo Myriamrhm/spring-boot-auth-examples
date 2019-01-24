3cb4bf4201e63c97741c5f0997e87eeec3640984
SecuDev <whatever>
2019-01-24 17:04:18 +0100

    Remove userdetails

---
+++ spring-boot-basic-auth/src/main/java/net/secudev/auth/security/CustomAuthenticationProvider.java
@@ -0,0 +1,71 @@
+package net.secudev.auth.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import net.secudev.auth.model.role.Role;
+import net.secudev.auth.model.utilisateur.IUtilisateurRepository;
+import net.secudev.auth.model.utilisateur.Utilisateur;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+	@Autowired
+	IUtilisateurRepository utilisateurs;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+		String username = authentication.getName().toString();
+
+		Utilisateur user = utilisateurs.findByLogin(username);
+
+		if (user == null) {
+			String msg = "Utilisateur non trouvé : "+username;
+			logger.trace(msg);
+			throw new UsernameNotFoundException(msg);
+
+		}
+
+		if (!passwordEncoder.matches((CharSequence) authentication.getCredentials(), user.getHashMotDePasse())) {
+			String msg = "Mauvais mot de passe pour "+username;
+			logger.trace(msg);
+			throw new BadCredentialsException(msg);
+		}
+
+		logger.trace("Authentification de : " + user.getLogin());
+
+		List<SimpleGrantedAuthority> roles = new ArrayList<SimpleGrantedAuthority>();
+
+		for (Role role : user.getRoles()) {
+			roles.add(new SimpleGrantedAuthority("ROLE_" + role.getLibelle()));
+			logger.trace("Role : " + role.getLibelle());
+		}
+
+		return new UsernamePasswordAuthenticationToken(user.getLogin(), user.getHashMotDePasse(), roles);
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
--- spring-boot-basic-auth/src/main/java/net/secudev/auth/security/SecurityConfig.java
+++ spring-boot-basic-auth/src/main/java/net/secudev/auth/security/SecurityConfig.java
@@ -3,12 +3,12 @@ package net.secudev.auth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,27 +16,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	private AuthenticationProvider authProvider;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authProvider);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and()
-		.httpBasic().and()
-		.cors().disable()
-		.csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);		
+		.httpBasic().and().cors().disable().csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-
-	
 }
