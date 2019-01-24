5e50bb8e9cb0efbcdff623145079e8a6af206696
SecuDev <whatever>
2019-01-24 22:59:21 +0100

    oops

---
+++ spring-boot-basic-auth/src/main/java/net/secudev/auth/annotation/Tracable.java
@@ -0,0 +1,14 @@
+package net.secudev.auth.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+//@PreAuthorize("hasRole('ROLE_admin')")
+@Documented
+public @interface Tracable {	
+}
